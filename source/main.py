3bcdc91cf94cb16e03df33a6bad16429ae0add1a
PimSchoolkateUPC <pim.schoolkate@estudiantat.upc.edu>
2022-03-15 10:50:49 +0100

    update on node uitls

--- source/main.py
+++ source/main.py
@@ -1,4 +1,5 @@
 from neo4j import GraphDatabase
+from nodebuild_utils import *
 
 driver = GraphDatabase.driver("neo4j://localhost:7687", auth=("neo4j", "louis"))
 
@@ -19,3 +20,22 @@ with driver.session() as session:
     session.read_transaction(print_friends, "Arthur")
 
 driver.close()
+
+
+test_labels = ("name", "age", "school")
+test_label_values = ("Pim", 24, "UPC")
+test_label_values2 = ("Louis", 21, "UPC")
+
+print(label_constructor(test_labels, test_label_values))
+
+test_node_1 = node_constructor("type", test_labels, test_label_values)
+test_node_2 = node_constructor("type", test_labels, test_label_values2)
+
+edge = edge_constructor("friend_of", labels=tuple(("quality", "extra")), label_values=tuple(("good", "spicy")))
+
+print(test_node_1.replace("__name__", "a"))
+driver = GraphDatabase.driver("neo4j://localhost:7687", auth=("neo4j", "louis"))
+with driver.session() as session:
+    #session.write_transaction(build_node, "type", test_labels, test_label_values)
+    #session.write_transaction(build_node, "type", test_labels, test_label_values2)
+    session.write_transaction(build_relation, test_node_1, test_node_2, edge)
--- source/nodebuild_utils.py
+++ source/nodebuild_utils.py
@@ -1,9 +1,8 @@
-from neo4j import GraphDatabase
-
 def label_constructor(labels, label_values):
     assert len(labels) == len(label_values)
     assert type(labels) == tuple
     assert type(label_values) == tuple
+
     label = "{"
 
     for i in range(0, len(labels)):
@@ -21,12 +20,6 @@ def label_constructor(labels, label_values):
     return label
 
 
-def build_node(tx, node_type, labels=(), label_values=()):
-    node = node_constructor(node_type, labels, label_values)
-    node = node.replace("__name__", 'a')
-    tx.run("CREATE {}".format(node))
-
-
 def node_constructor(node_type, labels=(), label_values=()):
     label = label_constructor(labels, label_values)
     return "(__name__:{} {})".format(node_type, label)
@@ -35,6 +28,11 @@ def node_constructor(node_type, labels=(), label_values=()):
 def edge_constructor(edge_type, labels=(), label_values=()):
     label = label_constructor(labels, label_values)
     return "[__name__:{} {}]".format(edge_type, label)
+
+
+def build_node(tx, node):
+    node = node.replace("__name__", 'a')
+    tx.run("CREATE {}".format(node))
 
 
 def build_relation(tx, n1, n2, e):
@@ -48,22 +46,3 @@ def build_relation(tx, n1, n2, e):
 def build_bi_relation(tx, n1, n2, e1, e2):
     build_relation(tx, n1, n2, e1)
     build_relation(tx, n2, n1, e2)
-
-
-test_labels = ("name", "age", "school")
-test_label_values = ("Pim", 24, "UPC")
-test_label_values2 = ("Louis", 21, "UPC")
-
-print(label_constructor(test_labels, test_label_values))
-
-test_node_1 = node_constructor("type", test_labels, test_label_values)
-test_node_2 = node_constructor("type", test_labels, test_label_values2)
-
-edge = edge_constructor("friend_of", labels=tuple(("quality", "extra")), label_values=tuple(("good", "spicy")))
-
-print(test_node_1.replace("__name__", "a"))
-driver = GraphDatabase.driver("neo4j://localhost:7687", auth=("neo4j", "louis"))
-with driver.session() as session:
-    #session.write_transaction(build_node, "type", test_labels, test_label_values)
-    #session.write_transaction(build_node, "type", test_labels, test_label_values2)
-    session.write_transaction(build_relation, test_node_1, test_node_2, edge)
