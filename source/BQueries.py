1f63a9d85cf6cb78ccf6de69fb0926e48985aa8b
PimSchoolkateUPC <pim.schoolkate@estudiantat.upc.edu>
2022-03-24 21:39:32 +0100

    B improvements and C queries

--- source/BQueries.py
+++ source/BQueries.py
@@ -30,13 +30,41 @@ def get_B(b):
         query = file.read()
     return query
 
-def transaction_function(tx, query):
-    tx.run(query)
+def transaction_B1(tx, query):
+    for record in tx.run(query):
+        print("")
+        print("")
+        print("Conference: \t" + record['conference'])
+        print("Date: \t \t" + record['date'])
+        print("First paper: \t" + record['first_paper'])
+        print("Times cited: \t" + str(record['times_cited_1']))
+        print("Second paper: \t" + record['second_paper'])
+        print("Times cited: \t" + str(record['times_cited_2']))
+        print("Third paper: \t" + record['third_paper'])
+        print("Times cited: \t" + str(record['times_cited_3']))
 
 
-def run_query(query, driver):
+def transaction_B2(tx, query):
+    for record in tx.run(query):
+        print(record)
+
+
+def transaction_B3(tx, query):
+    for record in tx.run(query):
+        print("")
+        print("Name of Journal: \t" + record["name"])
+        print("Year of the Journal: \t" + str(record['year']))
+        print("Impact factor: \t \t" + str(record['impact_factor']))
+
+
+def transaction_B4(tx, query):
+    for record in tx.run(query):
+        print("h-index: " + str(record['h_index']) + "\t Author: " + record["author"])
+
+
+def run_query(query, driver, function):
     with driver.session() as session:
-        session.write_transaction(transaction_function, query)
+        session.write_transaction(function, query)
 
 
 if __name__ == "__main__":
@@ -46,16 +74,20 @@ if __name__ == "__main__":
     if args.query == "B1":
         stat = "B1: Find the top 3 most cited papers of each conference:"
         query = get_B(args.query)
+        function = transaction_B1
     elif args.query == "B2":
         stat = "B2. For each conference and its community: i.e., those authors that have " \
                "published papers on that conference in, at least, 4 different editions:"
         query = get_B(args.query)
+        function = transaction_B2
     elif args.query == "B3":
         stat = "B3: Find the impact factors of the journals in your graph:"
         query = get_B(args.query)
+        function = transaction_B3
     elif args.query == "B4":
         stat = "B4: Find the h-indexes of the authors in your graph:"
         query = get_B(args.query)
+        function = transaction_B4
     else:
         raise ValueError("Could not find the proper query")
 
@@ -74,6 +106,6 @@ if __name__ == "__main__":
 
     driver = get_driver(args.driver_url, args.username, args.password)
 
-    run_query(query, driver)
+    run_query(query, driver, function)
 
     print("_"*100)
